9046af560f4a34ecf86e5aa310512139af5b434e
Aman Jain <jain.aman2608@gmail.com>
2023-05-29 19:30:03 +0530

    First commit

---
+++ js/client.js
@@ -0,0 +1,45 @@
+const socket = io("http://127.0.0.1:8000");
+// conA = io();
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messagecontainer = document.querySelector(".container");
+var audio = new Audio('Ding.mp3')
+
+const append=(message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messagecontainer.append(messageElement);
+    if(position="left"){
+        audio.play();
+    }
+}
+
+// Ask new user for his/her name and let the server know
+const name = prompt("Enter your name to join");
+socket.emit("new-user-joined", name);
+
+// If a new users joins, receive his/her name from the server
+socket.on('user-joined', name=>{
+append(`${name} joined the chat`, 'right');
+})
+
+// If server sends the message, receive it
+socket.on('receive', data=>{
+append(` ${data.name}: ${data.message}`, 'left')
+})
+
+// If a user leaves the chat, append the info to the container
+socket.on('left', name=>{
+append(`${name} left the chat`, 'left')
+})
+
+// If the form gets submitted, send server the message
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you:${message}`, "right")
+    socket.emit('send',message);
+    messageInput.value = "";
+})
